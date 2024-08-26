6a9436681af02baf21950f3e38ffe1b98b1f32dc
Dylan Tin <dylant711@gmail.com>
2024-08-26 00:44:37 -0400

    finished arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 100 + 1);
+
+let num3 = 60
+let num4 = 15
+const mod = num3 / num4;
+
+const max = Math.max(20);
